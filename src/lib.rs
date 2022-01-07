82992021d755385aeb9d7786c3875b4e2ff3a84e
Icelk <main@icelk.dev>
2022-01-07 21:16:53 +0100

    Added re-exports.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,11 @@
     // missing_docs
 )]
 pub mod index;
+pub mod proximity;
 pub mod query;
 pub mod set;
-pub mod proximity;
+
+pub use index::{
+    DocumentMap, Simple as SimpleIndex, SimpleOccurences as SimpleIndexOccurenceProvider,
+};
+pub use query::{Hit, Part, Query};
